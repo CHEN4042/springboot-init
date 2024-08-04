62ed6d1e325bfafb4cb59d10482889a434164032
CHEN4042 <chenhao404@icloud.com>
2024-08-04 09:45:56 +0800

    Initial commit

---
+++ src/main/java/com/haoc/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.haoc.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.haoc.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
